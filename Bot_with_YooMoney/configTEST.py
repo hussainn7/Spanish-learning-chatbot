d3b97e5e790ecba30e283384e0b4293524753254
Hussain <133125309+hussainn7@users.noreply.github.com>
2024-06-02 22:25:40 +0500

    Delete Bot_with_YooMoney directory

--- Bot_with_YooMoney/configTEST.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "7207723102:AAGG2GSf8gVr3TLH9WAIcIGU0mczacvAsas"
-
-PRICE = '950.00'
-
-information_about_company = 'We are wery good company, for exmpl'
-
-# ПЛАТЁЖ
-ACCOUNT_ID = 393037
-SECRET_KEY = 'test_b9DiYPsa4a6kV-YyfI4QM0CtiO_65o51x4946MujKAo'
