99af84bd0196e6888003c3c6c0b0c70ce62f5f06
Hussain <133125309+hussainn7@users.noreply.github.com>
2024-06-13 15:56:12 +0600

    Update and rename configTEST.py to payment.py

---
+++ payment.py
@@ -0,0 +1,12 @@
+TOKEN = "YOUR_TOKEN_FROM_TG"
+
+PRICE = '950.00' # TEST PRICE 950RUB
+
+information_about_company = 'We are wery good company, for exmpl' #Example 
+
+
+ACCOUNT_ID = 111111 #Your YooMoney MerchantID
+SECRET_KEY = 'test_some_large_random_letters' # Your secret merchantSK YooMoney
+
+
+
