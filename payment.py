05f85c4bda5e2c93c404e0d7154e39e1fc040907
Hussain <133125309+hussainn7@users.noreply.github.com>
2024-06-13 15:58:14 +0600

    Update and rename paymentTEST.py to payment.py

--- payment.py
+++ payment.py
@@ -1,7 +1,7 @@
 import yookassa
 from yookassa import Payment
 import uuid
-from configTEST import ACCOUNT_ID, SECRET_KEY
+from config import ACCOUNT_ID, SECRET_KEY
 
 yookassa.Configuration.account_id = ACCOUNT_ID
 yookassa.Configuration.secret_key = SECRET_KEY
@@ -10,8 +10,8 @@ def create(amount, chat_id):
     id_key = str(uuid.uuid4())
     payment = Payment.create({
         'amount': {
-            'value': str(amount),  # Convert amount to string
-            'currency': 'RUB'
+            'value': str(amount),  
+            'currency': 'RUB' #Adjust as you need exmpl: USD, RUB...
         },
         'payment_method_data': {
             'type': 'bank_card'
